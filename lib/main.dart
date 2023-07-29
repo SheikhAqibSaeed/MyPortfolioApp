a85b15d6816ac09a58c28fc8f80f2b34af68fd58
Aqib <aqibsaeed570@gmail.com>
2023-07-29 21:13:59 +0500

    ---My Portfolio App--- Added SizeBox. Added Icons. Added Axiz Aligment. Added StyleSize.

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,67 @@ class Home extends StatelessWidget {
             Row(
               children: <Widget> [
                 CircleAvatar(radius: 60,),
+                SizedBox(width: 50,),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget> [
-                    Text("UserName"),
-                    Text("Desigination")
+                    Text("UserName" ,style: TextStyle(fontSize: 35),),
+                    Text("Desigination",style: TextStyle(fontSize: 17.5),)
                   ],
                 )
               ],
-            )
+            ),
+            SizedBox(height: 40,),
+            Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Column(
+                children: <Widget> [
+                  Row(
+                    children: <Widget> [
+                    Icon(Icons.school_outlined, size: 40,),
+                      SizedBox(width: 20,),
+                      Text("School Name",style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget> [
+                      Icon(Icons.computer, size: 40,),
+                      SizedBox(width: 20,),
+                      Text("School Name",style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget> [
+                      Icon(Icons.location_on, size: 40,),
+                      SizedBox(width: 20,),
+                      Text("School Name",style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget> [
+                      Icon(Icons.email, size: 40,),
+                      SizedBox(width: 20,),
+                      Text("School Name",style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget> [
+                      Icon(Icons.phone, size: 40,),
+                      SizedBox(width: 20,),
+                      Text("School Name",style: TextStyle(fontSize: 20),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text("About Me"),
+            
+            Text("Created By Aqib Saeed")
           ],
         ),
       ),
